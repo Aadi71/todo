b1cfb3c455e5ae6b0c692d9c18a0a883b87008d0
Aadi71 <jainaadi7102@gmail.com>
2023-03-23 00:38:18 +0530

    added ability

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -13,6 +13,10 @@ class TodoItemsController < ApplicationController
         end
         redirect_to @todo_list 
     end
+    def complete
+        @todo_item.update_attribute(:completed_at, Time.now)
+        redirect_to @todo_list, notice: "Todo item completed"
+    end
     private
     def set_todo_list
         @todo_list = TodoList.find(params[:todo_list_id])
